8829aac14259e039caf2817fade86ff6fc43da7e
Kizzzy2 <107805550+Kizzzy2@users.noreply.github.com>
2026-04-11 12:04:48 -0500

    Add App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Home from './pages/Home'
+import Blog from './pages/Blog'
+import BlogPost from './pages/BlogPost'
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/blog/:slug" element={<BlogPost />} />
+      </Routes>
+      <Footer />
+    </>
+  )
+}
+
+export default App
