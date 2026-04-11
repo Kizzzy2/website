4c4c5250b3b595c0bc01b6cfa40fbc9b9dcf5be3
Kizzzy2 <107805550+Kizzzy2@users.noreply.github.com>
2026-04-11 13:01:58 -0500

    Add all page routes to App

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,29 @@ import { Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Home from './pages/Home'
+import Services from './pages/Services'
+import CeramicCoating from './pages/CeramicCoating'
+import Pricing from './pages/Pricing'
+import Gallery from './pages/Gallery'
+import About from './pages/About'
+import Areas from './pages/Areas'
+import Fleet from './pages/Fleet'
 import Blog from './pages/Blog'
 import BlogPost from './pages/BlogPost'
 
-function App() {
+export default function App() {
   return (
     <>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/ceramic-coating" element={<CeramicCoating />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/areas" element={<Areas />} />
+        <Route path="/fleet" element={<Fleet />} />
         <Route path="/blog" element={<Blog />} />
         <Route path="/blog/:slug" element={<BlogPost />} />
       </Routes>
@@ -18,5 +32,3 @@ function App() {
     </>
   )
 }
-
-export default App
